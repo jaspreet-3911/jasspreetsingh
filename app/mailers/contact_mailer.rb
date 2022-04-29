763e605d6f482af7d69a97f855ef6efd4838fb0f
Jaspreet Singh <singh.jaspreet915@gmail.com>
2022-04-29 21:52:23 +0530

    add captcha

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
 
   def contact_email
     @contact = params[:contact]
-    mail(to: 'singh.jaspreet915@gmail.com', subject: "[Personal Portfolio] Message from your personal website")
+    mail(to: ENV['EMAIL_TO'], subject: "[#{ENV['HOST']}] Message from your personal website")
   end
 end
